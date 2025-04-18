ee46584d81c946e5f56f16610cb59175e8c9eada
NikoKenara <nikokenara@gmail.com>
2025-04-18 13:49:42 +0700

    Laravel User Authentication with Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\TestControl;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,88 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
-
-Route::get('about', function(){
-    // return 'ini abotu page';
-    return view('info.about');
 });
 
-Route::get('contact', function(){
-    return [
-        'name' => 'jhon deo',
-        'age' => '24',
-        'location' => 'usa'
-    ];
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/* Route::post();
-
-/**
- * ___________________________________________________________________
- * ROUTE METHODS
- * ___________________________________________________________________
- * */
-/**
- * 1. GET
- * 2. POST
- * 3. PUT
- * 4. PATCH
- * 5. DELETE
- */
-
-/**
- * ___________________________________________________________________
- * ROUTE PARAMETERS
- * ___________________________________________________________________
- * */
-
-
-/**Route::get('person/{id_of_person}', function(){
-    return 'kenara';
-});
-*/
-
-Route::get('person/{id?}', function($id = NULL){
-    return [
-        'id' => $id,
-        'name' => 'kenara',
-    ];
-});
-
-// Route::get('person/{id}/post/{post_id}', function($id, $postId){
-//     return [
-//         'id' => $id,
-//         'name' => 'kenara',
-//         'post_id' => $postId
-//     ];
-// });
-
-
-Route::get('animals', function(){
-    return 'anjing dan kucing';
-})->name('animals');
-
-Route::group(['as'=> 'animals.', 'prefix' => 'animals'], function(){
-    Route::get('dog', function(){
-        return 'guk-guk!';
-    })->name('anjing');
-
-    Route::get('cat', function(){
-        return 'miaw!';
-    })->name('kucing');
-
-    Route::get('monkey', function(){
-        return 'ho ho!';
-    })->name('monyet');
-});
-
-Route::get('blade-test', function() {
-    return view('blade-test');
-})->name('blade-test');
-
-Route::get('contact', [TestControl::class, 'contact'])->name('contact');
-Route::get('contact/store', [TestControl::class, 'store'])->name('contact.store');
-
-
-Route::resource('blog', BlogController::class)->middleware('auth.check');
+require __DIR__.'/auth.php';
